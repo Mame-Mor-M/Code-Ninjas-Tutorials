a190dc015bd1f3facb4ba4a800a9f685007377c0
Mame Mor Mbacke <60527044+Mame-Mor-M@users.noreply.github.com>
2025-02-28 01:46:44 -0500

    0.0.70

--- assets/js/binary.js
+++ assets/js/binary.js
@@ -1,4 +1,4 @@
-// meta={"simUrl":"https://trg-arcade.userpxt.io/v1.12.59/---simulator","cdnUrl":"https://cdn.makecode.com","version":"0.0.69","target":"arcade","targetVersion":"1.12.59","repo":"mame-mor-m/code-ninjas-tutorials"}
+// meta={"simUrl":"https://trg-arcade.userpxt.io/v1.12.59/---simulator","cdnUrl":"https://cdn.makecode.com","version":"0.0.70","target":"arcade","targetVersion":"1.12.59","repo":"mame-mor-m/code-ninjas-tutorials"}
 // total=1296630 new=8.20% cached=86.29% other=5.51%
 (function (ectx) {
 'use strict';
@@ -429,7 +429,7 @@ const pxsim_pxtrt = pxsim.pxtrt;
 const pxsim_numops = pxsim.numops;
 
 
-function _main___P226181(s) {
+function _main___P226324(s) {
 let r0 = s.r0, step = s.pc;
 s.pc = -1;
 
@@ -893,13 +893,13 @@ switch (step) {
     return leave(s, r0)
   default: oops()
 } } }
-_main___P226181.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"main.ts","functionName":"<main>","argumentNames":[]}
-_main___P226181.continuations = [  ]
+_main___P226324.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"main.ts","functionName":"<main>","argumentNames":[]}
+_main___P226324.continuations = [  ]
 
-function _main___P226181_mk(s) {
+function _main___P226324_mk(s) {
     checkStack(s.depth);
     return {
-        parent: s, fn: _main___P226181, depth: s.depth + 1,
+        parent: s, fn: _main___P226324, depth: s.depth + 1,
         pc: 0, retval: undefined, r0: undefined, overwrittenPC: false, lambdaArgs: null,
   tmp_0: undefined,
   tmp_1: undefined,
@@ -43224,5 +43224,5 @@ const sprites_BaseSpriteSayRenderer__C210137_VT = mkVTable({
 
 const breakpoints = setupDebugger(1, ["transparency16___212210","tile1___212211","tile2___212212","tile3___212213","tile4___212214","tile5___212215","tile6___212216","tile7___212217","tile9___212218","tile10___212219","tile11___212220","tile12___212221","tile8___212222"])
 
-return _main___P226181
+return _main___P226324
 })
